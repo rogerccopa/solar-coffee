9713a0bcb1ede30899fa104fd18d30042471fb75
Roger Ccopa <rogerccopa@Rogers-MacBook-Pro.local>
2021-04-09 16:27:20 -0500

    Created frontend app layout

--- solarcoffee.frontend/src/router/index.ts
+++ solarcoffee.frontend/src/router/index.ts
@@ -1,24 +1,20 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Inventory from "@/components/Inventory.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'Home',
+    component: Inventory
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    path: '/inventory',
+    name: 'Inventory',
+    component: Inventory
+  }
 ];
 
 const router = new VueRouter({
